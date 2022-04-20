6eb6e5b0f5596c34a674ff2f86f38d2089948007
MFarooqKhalid <mukhalid@unifonic.com>
2022-04-20 20:54:48 +0500

    Update two tests in suite one

--- src/test/java/suite1/TestHelloWorld.java
+++ src/test/java/suite1/TestHelloWorld.java
@@ -1,16 +1,27 @@
 package suite1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class TestHelloWorld {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Test Hello World");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.get("https://jqueryui.com/slider/");
     }
 
     @Test
     public void testHelloProgramming(){
-        System.out.println("Test Hello Programming");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.get("https://google.com");
     }
 }
